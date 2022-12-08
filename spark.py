d8101a0f667980a80fc855f0922bdb2fe9deeba1
alfredolandi97 <alfredo.landi@mail.polimi.it>
2022-12-08 18:20:14 +0100

    some fixes

--- spark.py
+++ spark.py
@@ -2,10 +2,11 @@ import findspark
 findspark.init()
 import pyspark
 import csv
+import datetime
 
 #create a spark session
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.master("local").appName("MySecondSparkApp").getOrCreate()
+spark = SparkSession.builder.master("local").appName("Queries").getOrCreate()
 
 #article csv loading...
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType
@@ -13,23 +14,35 @@ schemaArticle = StructType([
     StructField("key", StringType(), False),
     StructField("title", StringType(), True),
     StructField("mdate", DateType(), True),
-    StructField("year", StringType(), True),
+    StructField("year", IntegerType(), True),
     StructField("month", StringType(), True),
     StructField("cdrom", StringType(), True),
     StructField("url", StringType(), True),
     StructField("ee", StringType(), True),
-    StructField("number", StringType(), True),
+    StructField("number", IntegerType(), True),
     StructField("volume", StringType(), True),
     StructField("pages", StringType(), True),
     StructField("journal", StringType(), True)
 ])
 
 articles_csv = open("./resources/articles.csv", newline="", encoding="utf-8")
-df_data_articles = csv.reader(articles_csv, delimiter=";")
-#input("premi un tast per continuare")
+articles_reader = csv.reader(articles_csv, delimiter=";")
+
+
+#data parsing...
+df_data_articles = []
+
+for row in articles_reader:
+    row[2] = datetime.datetime.strptime(row[2], '%Y-%m-%d')
+    row[3] = int(row[3])
+    row[8] = int(row[8])
+    df_data_articles.append(row)
+
 df_article = spark.createDataFrame(data = df_data_articles, schema=schemaArticle)
 df_article.printSchema()
 df_article.show()
+
+input("premi un tast per continuare")
 
 #query 2: show the first 5 results about article's titles which contains "comput" inside
 df_article.filter(df_article.title.like("%comput%")).limit(5).show(truncate=False)
